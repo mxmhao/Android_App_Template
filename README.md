66204d6e0f9f1ad61ef3e756e61745ab8d8a7ddd
mxmhao <mxmhao@163.com>
2019-01-24 15:14:47 +0800

    添加获取本地图片或视频的缩略图的模板

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 一些安卓App工具类和模板代码，引入到自己的项目时可能会报错，可自行修改
 
 ## [工具类，在“/app/src/main/java/utils/”目录下](https://github.com/mxmhao/Android_App_Template/tree/master/app/src/main/java/utils)
-1. [获取全局Context的工具类，无须传入Context —— get_context_no_dependence_anything/](https://github.com/mxmhao/Android_App_Template/tree/master/app/src/main/java/utils/get_context_no_dependence_anything)
+1. [获取全局Context的工具类，无须传入Context：get_context_no_dependence_anything/](https://github.com/mxmhao/Android_App_Template/tree/master/app/src/main/java/utils/get_context_no_dependence_anything)
 
 ## [模板，在“/app/src/main/java/template/”目录下](https://github.com/mxmhao/Android_App_Template/tree/master/app/src/main/java/template)
-1. [RecyclerView分组的模板代码 —— recyclerview_group/](https://github.com/mxmhao/Android_App_Template/tree/master/app/src/main/java/template/recyclerview_group)
-2. [自定义HUD指示器模板代码，可以禁止用户交互和允许用户交互 —— ProgressHUD.java](https://github.com/mxmhao/Android_App_Template/blob/master/app/src/main/java/template/ProgressHUD.java)
+1. [RecyclerView分组的模板代码：recyclerview_group/](https://github.com/mxmhao/Android_App_Template/tree/master/app/src/main/java/template/recyclerview_group)
+2. [自定义HUD指示器模板代码，可以禁止用户交互和允许用户交互：ProgressHUD.java](https://github.com/mxmhao/Android_App_Template/blob/master/app/src/main/java/template/ProgressHUD.java)
+3. [获取本地图片或视频的缩略图：ThumbnailImage.java](https://github.com/mxmhao/Android_App_Template/blob/master/app/src/main/java/template/ThumbnailImage.java)
---
+++ app/src/main/java/template/ThumbnailImage.java
@@ -0,0 +1,116 @@
+package template;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class ThumbnailImage extends Fragment {
+
+    private Context mContext;
+    ContentResolver cr;
+    private ImageView imageView;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.mContext = context;
+        cr = mContext.getContentResolver();
+        imageView = null;
+    }
+
+    //测试获取缩略图
+    public void test() {
+        File file = new File("");
+        String path = file.getAbsolutePath();
+        //1、根据文件路径获取缩略图的id
+        if (isPhoto(path)) {
+            long id = getMediaId(cr, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, path);
+
+            LoadThumbnailsAsyncTask ltat = new LoadThumbnailsAsyncTask(cr);
+            ltat.execute(imageView, id, 1);
+        } else if (isVideo(path)) {
+            long id = getMediaId(cr, MediaStore.Video.Media.EXTERNAL_CONTENT_URI, path);
+
+            LoadThumbnailsAsyncTask ltat = new LoadThumbnailsAsyncTask(cr);
+            ltat.execute(imageView, id, 2);
+        }
+    }
+
+    private static final String WHERE = MediaStore.MediaColumns.DATA + "=?";//文件的绝对路径
+    private static final String[] COLUMNS = new String[]{MediaStore.MediaColumns._ID};//图片id
+
+    private static long getMediaId(ContentResolver cr, Uri content_uri, String path) {
+        Cursor cursor = cr.query(content_uri, COLUMNS, WHERE, new String[]{path}, null);
+        if (cursor.moveToFirst()) {
+            long id = cursor.getLong(0);
+            cursor.close();
+            return id;
+        }
+
+        return -1;
+    }
+
+    public boolean isPhoto(String path) {
+        return path.endsWith(".png");
+    }
+
+    public boolean isVideo(String path) {
+        return path.endsWith(".mp4");
+    }
+
+    //异步获取缩略图
+    static class LoadThumbnailsAsyncTask extends AsyncTask<Object, Void, Bitmap> {
+        private ImageView imageView;
+        ContentResolver cr;
+
+        public LoadThumbnailsAsyncTask(ContentResolver cr) {
+            this.cr = cr;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Object... params) {
+            //这是在后台子线程中执行的
+            imageView = (ImageView) params[0];
+            int id = (int) params[1];
+            int type = (int) params[2];
+            Bitmap bitmap = null;
+            //2、根据缩略图id获取缩略图
+            if (1 == type) {
+                bitmap = MediaStore.Images.Thumbnails.getThumbnail(cr, id, MediaStore.Images.Thumbnails.MINI_KIND, null);
+            } else if (2 == type) {
+                bitmap = MediaStore.Video.Thumbnails.getThumbnail(cr, id, MediaStore.Video.Thumbnails.MINI_KIND, null);
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            //当任务执行完成是调用,在UI线程
+            //取消后这里不会执行
+            imageView.setImageBitmap(bitmap);
+            imageView.setTag(null);
+            imageView = null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            imageView = null;
+        }
+
+        @Override
+        protected void onCancelled(Bitmap bitmap) {
+            super.onCancelled(bitmap);
+            imageView = null;
+        }
+    }
+}
