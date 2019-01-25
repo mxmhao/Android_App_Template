268a0351db6f5f3f297a80b03cd205aca66435f1
mxmhao <mxmhao@163.com>
2019-01-25 18:32:49 +0800

    添加注释

--- app/src/main/java/template/BottomNavigationFragment.java
+++ app/src/main/java/template/BottomNavigationFragment.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+//https://www.jianshu.com/p/24278f3259b3
 //https://stackoverflow.com/questions/40176244/how-to-disable-bottomnavigationview-shift-mode
 public class BottomNavigationFragment extends Fragment {
 
